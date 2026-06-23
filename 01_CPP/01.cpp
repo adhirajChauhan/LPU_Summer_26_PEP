cbb99e89fc1841cd00a83918613b90596430c75c
adhirajChauhan <adhirajsingh0105@gmail.com>
2026-06-23 09:24:43 +0530

    23062026

---
+++ 01_CPP/01.cpp
@@ -0,0 +1,97 @@
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+ 
+ //Data types and Variables
+    // int age = 10; 
+    // char name = 'A';
+    // bool ans = true;
+
+    //int  -> 4 byte
+    //long long -> 8 byte
+    // float -> 4 byte
+    // double -> 8 byte
+
+    // char -> 1 byte
+    //bool -> 1 byte
+
+
+    // cout << age << endl;
+
+
+
+    //------------------------------
+
+    //Conditional statements
+    // if(cond.){
+        //if cond is true, this block will be executed
+    // }
+    // else{
+        //otherwise this will be executed
+    // }
+    // int age =20;
+    // int number = 15;
+
+    // if(age >= 18 && number >10){
+    //     cout << "1" << endl;
+    // }
+    // else if(age > 18 && number < 18){
+    //     cout << "2" << endl;
+    // }
+    // else{
+    //     cout << "3" << endl;
+    // }
+
+
+    //switch
+    // char grade = 'A';
+
+    // switch(grade){
+    //     case 'A':
+    //     cout << "Excellent" << endl;
+    //     break;
+    //     case 'B':
+    //     cout << "Good" << endl;
+    //     break;
+    //     case 'C':
+    //     cout << "Average" << endl;
+    //     break;
+    //     default:
+    //     cout << "Invalid" << endl;
+    // }
+    
+
+    // int x = 10, y = 20;
+
+    // int ans = (x > y) ? x : y;
+    
+    //-------------------
+
+    // for loop
+
+    // for(init ; terminate ; inc/dec){
+
+    // }
+
+    int i = 0;
+
+    // for(i = 0; i < 10; i++){
+    //     cout << i << " ";
+    // }
+
+    // while(i < 5){
+    //     cout << i << " ";
+    //     i++;
+    // }
+
+    do{
+        cout << i << " ";
+        i++;
+    }
+    while(i < 5);
+
+
+
+}
