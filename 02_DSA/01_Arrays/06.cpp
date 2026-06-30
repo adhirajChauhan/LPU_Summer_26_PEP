bb0b8939f7788ef39dde7c20e4489851be2756c9
adhirajChauhan <adhirajsingh0105@gmail.com>
2026-06-30 09:44:34 +0530

    30062026

--- 02_DSA/01_Arrays/06.cpp
+++ 02_DSA/01_Arrays/06.cpp
@@ -203,13 +203,69 @@ class Solution {
 
 
 
+https://leetcode.com/problems/maximum-subarray/description/
+
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currSum = 0;
+        int maxSum = nums[0];
+
+        for(int i = 0; i < nums.size(); i++){
+            currSum += nums[i];
+
+            if(currSum > maxSum) maxSum = currSum;
+
+            if(currSum < 0) currSum = 0;
+        }
+        return maxSum;
+    }
+};
 
 
 
+https://www.geeksforgeeks.org/problems/reverse-array-in-groups0255/1
+
+class Solution {
+  public:
+    void reverseInGroups(vector<int> &arr, int k) {
+        // code here
+        int n = arr.size();
+        for(int i = 0; i < n; i += k){
+            int start = i;
+            int end = min(i + k -1, n -1 );
+            
+            while(start < end){
+                swap(arr[start++], arr[end--]);
+            }
+        }
+    }
+};
 
 
+https://www.geeksforgeeks.org/problems/max-sum-subarray-of-size-k5313/1
 
-
+class Solution {
+  public:
+    int maxSubarraySum(vector<int>& arr, int k) {
+        // code here
+        int n = arr.size();
+        int currSum = 0;
+        int maxSum = 0;
+        
+        for(int i = 0; i < k; i++){
+            currSum += arr[i];
+        }
+        maxSum = currSum;
+        
+        for(int i = k; i < n; i++){
+            currSum = currSum - arr[i - k] + arr[i];
+            if(currSum > maxSum) maxSum = currSum;
+        }
+        return maxSum;
+    }
+};
 
 
 
