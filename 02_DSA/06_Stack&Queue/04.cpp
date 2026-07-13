f1b06dab3d5fb7e7a5f1c01f279b340cf887a034
adhirajChauhan <adhirajsingh0105@gmail.com>
2026-07-13 13:57:16 +0530

    13072026

--- 02_DSA/06_Stack&Queue/04.cpp
+++ 02_DSA/06_Stack&Queue/04.cpp
@@ -22,3 +22,100 @@ int main()
 {
  
 }
+
+
+https://leetcode.com/problems/valid-parentheses/
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(char ch : s){
+            if(ch == '(' || ch =='{' || ch == '['){
+                st.push(ch);
+            }
+            else{
+                if(st.empty()) return false;
+
+                char top = st.top();
+                if(ch == ')' && top == '(' ||ch == '}' && top == '{' ||ch == ']' && top == '['){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
+
+
+https://www.geeksforgeeks.org/problems/insert-an-element-at-the-bottom-of-a-stack/1
+
+class Solution {
+  public:
+    stack<int> insertAtBottom(stack<int> st, int x) {
+        //base case
+        if(st.empty()){
+            st.push(x);
+            return st;
+        }
+        int topVal = st.top();
+        st.pop();
+        st = insertAtBottom(st, x);
+        st.push(topVal);
+        
+        return st;
+        
+    }
+};
+
+
+https://www.geeksforgeeks.org/problems/queue-reversal/1
+
+class Solution {
+  public:
+    void reverseQueue(queue<int> &q) {
+        // code here
+        if(q.empty()) return;
+        
+        int temp = q.front();
+        q.pop();
+        reverseQueue(q);
+        q.push(temp;)
+        
+        
+    }
+};
+
+
+https://www.geeksforgeeks.org/problems/reverse-first-k-elements-of-queue/1
+
+class Solution {
+  public:
+    queue<int> reverseFirstK(queue<int> q, int k) {
+        // code here
+        stack<int> ast;
+        
+        if(k > q.size()) return q;
+        //run a loop for k elements and push them in stack
+        for(int i = 0; i < k; i++){
+            st.push(q.front());
+            q.pop();
+        }
+        //run a loop until st is empty and push all the elements back in queue
+        while(!st.empty()){
+            q.push(st.top());
+            st.pop();
+        }
+        //run a loop till size - k, pop all the elements of queue and push them back in queue again
+        for(int i = 0; i < q.size() - k; i++){
+            q.push(q.front());
+            q.pop();
+        }
+        return q;
+        
+    }
+};
