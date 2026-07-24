9a9ac233f32a6f834dd48d6f9afec18869832efa
adhirajChauhan <adhirajsingh0105@gmail.com>
2026-07-24 16:54:49 +0530

    24072026

--- 09_Graphs/03.cpp
+++ 09_Graphs/03.cpp
@@ -216,3 +216,155 @@ class Solution {
         return false;
     }
 };
+
+
+https://leetcode.com/problems/flood-fill/
+
+class Solution {
+public:
+
+    void dfs(int r, int c, vector<vector<int>>& image, int originalColor, int newColor){
+        int n = image.size();
+        int m = image[0]. size();
+
+        if(r < 0 || r >= n || c < 0 || c >= m) return;
+        if(image[r][c] != originalColor) return;
+        image[r][c] = newColor;
+
+        //top
+        dfs(r-1, c, image, originalColor, newColor);
+        //bottom
+        dfs(r+1, c, image, originalColor, newColor);
+        //left
+        dfs(r, c-1, image, originalColor, newColor);
+        //right
+        dfs(r, c+1, image, originalColor, newColor);
+
+
+
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int originalColor = image[sr][sc];
+        if(originalColor == color) return image;
+        dfs(sr, sc, image, originalColor, color);
+        return image;
+
+    }
+};
+
+
+https://www.geeksforgeeks.org/problems/topological-sort/1
+
+class Solution {
+  public:
+  void dfs(int node , vector<vector<int>> &adj, vector<bool> &visited, stack<int> &st){
+      visited[node] = true;
+      
+      for(auto neighbour : adj[node]){
+          if(!visited[neighbour]){
+              dfs(neighbour, adj, visited, st);
+          }
+      }
+      st.push(node);
+  }
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+        // code here
+        vector<vector<int>> adj(V);
+        for(auto i : edges){
+            int u = i[0];
+            int v = i[1];
+            
+            adj[u].push_back(v);
+        }
+        
+        vector<bool> visited(V, false);
+        stack<int> st;
+        
+        for(int i = 0; i < V; i++){
+            if(!visited[i]){
+                dfs(i, adj, visited, st);
+            }
+        }
+        
+        vector<int> result;
+        while(!st.empty()){
+            result.push_back(st.top());
+            st.pop();
+        }
+        return result;
+        
+    }
+};
+
+
+
+
+
+
+
+https://www.geeksforgeeks.org/problems/topological-sort/1
+
+class Solution {
+  public:
+    vector<int> topoSort(int V, vector<vector<int>>& edges) {
+        // code here
+        //Kahn's algo
+    //compute indegree of every node
+    //push all the nodes with indegree = 0 in the queue
+    //remove nodes one by one fro queue, and add to the answer
+    // for each removed node, decrease the indegree if its neighbour by 1
+    // if any neighbour becomes indegree == 0, push that in queuue
+    //if number opf visitedNode != number of given vertices -> cycle exist
+    
+    vector<vector<int>> adj(V);
+    for(auto i : edges){
+        int u = i[0];
+        int v = i[1];
+        adj[u].push_back(v);
+        
+        //create a indegree vector
+        vector<int> indegree(V, 0);
+        for(int i = 0; i < V; i++){
+            for(int j : adj[i]){
+                indegree[j]++;
+            }
+        }
+        //push all vertices with indegree 0 in queue
+        queue<int> q;
+        for(int i = 0; i < V; i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        vector<int> result;
+        while(!q.empty()){
+            int front = q.front();
+            q.pop();
+            result.push_back(front);
+            
+            for(auto neighbour : adj[front]){
+                indegree[neighbour]--;
+                
+                if(indegree[neighbour] == 0){
+                    q.push(neighbour);
+                }
+            }
+        }
+        if(result.size() != V){
+            return {};
+        }
+        return result;
+        
+    }
+    
+    
+    }
+};
+
+
+
+
+
+
+
+
